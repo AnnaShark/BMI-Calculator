0b0b525a17793fa860c5d922cd5f9f037aef81f7
AnnaShark <30521253+AnnaShark@users.noreply.github.com>
2021-08-24 16:36:19 +0200

    Add files via upload

---
+++ BMI Calculator/Views/ResultsViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ResultsViewController.swift
+//  BMI Calculator
+//
+//  Created by Anna Shark on 24/8/21.
+//  Copyright © 2021 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+   
+    var bmiValue: String?
+    var advice: String?
+    var color: UIColor?
+    
+    @IBOutlet weak var bmiLabel: UILabel!
+    @IBOutlet weak var adviceLabel: UILabel!
+    
+//    override var modalPresentationStyle: UIModalPresentationStyle {
+//        get { .fullScreen }
+//        set { assertionFailure("Shouldnt change that") }
+//    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        bmiLabel.text = bmiValue
+        adviceLabel.text = advice
+        view.backgroundColor = color
+
+        
+    }
+    @IBAction func recalculatePressed(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
